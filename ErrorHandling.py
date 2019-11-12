4eb63b1ab98b33a91af1f4bdf5e4b2fe25551bc7
catrinehom <48596773+catrinehom@users.noreply.github.com>
2019-11-12 15:39:59 +0100

    Update ErrorHandling.py

--- ErrorHandling.py
+++ ErrorHandling.py
@@ -1,24 +1,11 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-#Program: SegmentIdentifier
-#Description: This program can find ID corresponding to segments from an input database in your MinION data.
+Program: ContigExtractor
+Description: This program can find ID corresponding to contigs from a input references in your MinION data.
 Step 0: This is the error handling of the step 'Get input'
-#Version: 1.0
-#Author: Catrine Ahrens Høm
-
-#Usage:
-    ## SegmentIdentifier.sh [-i] <MinION reads in fastq format> [-d] <Database of wanted segments in fasta format>
-    ## -i --input: MinION filename path relative to current working directory.
-    ## -d --database: Database filename path relative to current working directory.
-    ## ID.txt will be available in the working directory after run.
-
-#This pipeline consists of 5 steps:
-    ## STEP 1:  Unicycler (Skipped if assembly file is supplied)
-    ## STEP 2:  Find the segments which match database
-    ## STEP 3:  Choose this segment in fsa fragment
-    ## STEP 4:  KMA fragment against reads
-    ## STEP 5:  Find IDs and save to file
+Version: 1.0
+Author: Catrine Ahrens Høm
 """
 
 # Import libraries 
@@ -101,22 +88,22 @@ def CheckFasta(filenames):
 # Parse input from command line 
 parser = ArgumentParser()
 parser.add_argument('-i', '--input', dest='input_fastq', help='Fastq file', nargs = '+')
-parser.add_argument('-d', '--database', dest='db',help='Database of sequence you want to examine', nargs = '+')
-parser.add_argument('-ug', dest='ug',help='Unicycler assembly.gfa file')
-parser.add_argument('-uf', dest='uf',help='Unicycler assembly.fasta file')
+parser.add_argument('-r', '--reference', dest='db',help='References you want to examine', nargs = '+')
+parser.add_argument('-g', dest='g',help='Unicycler assembly.gfa file')
+parser.add_argument('-f', dest='f',help='Unicycler assembly.fasta file')
 args = parser.parse_args()
 
 # Define input as variables
 input_fastq = args.input_fastq
 db = args.db
 
-if args.ug:
-    ug = args.ug
+if args.g:
+    g = args.g
     
-if args.uf:
-    uf = args.uf
+if args.f:
+    f = args.f
 
-# Test if fasta database and fastq files is exists in folder  
+# Test if fasta references and fastq files is exists in folder  
 for file in input_fastq:
     if os.path.exists(file) == False:
         sys.exit('Input Error: '+file+' does not exist in path.')   
@@ -125,13 +112,13 @@ for file in db:
     if os.path.exists(file) == False:
         sys.exit('Input Error: '+file+' does not exist in path.')
     
-# Test if database and input files is correct format
+# Test if references and input files is correct format
 db_check_fasta = CheckFasta(db)
 
 input_check_fasta = CheckFasta(input_fastq)
 input_check_fastq = CheckFastq(input_fastq)
 
-# Database should be in fasta format
+# References should be in fasta format
 for i in range(0,len(db_check_fasta)):
     if db_check_fasta[i] is False:
         sys.exit('Input Error: '+db[i]+' is a wrong format. Should be fasta format.')
@@ -142,11 +129,11 @@ for i in range(0,len(input_check_fasta)):
         sys.exit('Input Error: '+input_fastq[i]+' is a wrong format. Should be fastq or fasta format.')
  
 # Test Unicycler input if given
-if args.ug:
-    if os.path.exists(ug) == False:
-        sys.exit('Input Error: '+ug+' does not exist in path.')
-    if os.path.exists(uf) == False:
-        sys.exit('Input Error: '+uf+' does not exist in path.')
+if args.g:
+    if os.path.exists(g) == False:
+        sys.exit('Input Error: '+g+' does not exist in path.')
+    if os.path.exists(f) == False:
+        sys.exit('Input Error: '+f+' does not exist in path.')
     
 
     ### Check that the input is gfa and fasta
@@ -154,25 +141,25 @@ if args.ug:
     fasta_type = '>'
     gfa_type = 'S'
  
-    # Open uf file and get the first character
-    f = open(uf, 'r') 
-    first_char = f.read(1);
-    f.close()
+    # Open f file and get the first character
+    file = open(f, 'r') 
+    first_char = file.read(1);
+    file.close()
     
-    # Check if fsa
+    # Check if fasta
     if first_char != fasta_type:
-        print('Error! Unknown format of input file: '+uf+'. Should be fasta format')
+        print('Error! Unknown format of input file: '+f+'. Should be fasta format')
         sys.exit(1)
     
     
-    # Open ug file and get the first character
-    f = open(ug, 'r')
-    first_char = f.read(1);
-    f.close()
+    # Open g file and get the first character
+    file = open(g, 'r')
+    first_char = file.read(1);
+    file.close()
     
     # Check if gfa
     if first_char != gfa_type:
-        print('Error! Unknown format of input file: '+ug+'. Should be gfa format')
+        print('Error! Unknown format of input file: '+g+'. Should be gfa format')
         sys.exit(1)
     
 
@@ -181,7 +168,7 @@ if args.ug:
 # Check input files from command line, else ask for it
 if args.input is None:
     filename = input('Please choose a assembly file to load: ')
-    plasmidfile = input('Please choose a plasmid database to load: ')
+    plasmidfile = input('Please choose a plasmid references to load: ')
 else:
     filename = args.input
     plasmidfiles = args.db
