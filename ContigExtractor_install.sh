6e4fa4f7a7f51304c3cf0a9c3a2f35653f04a381
catrinehom <48596773+catrinehom@users.noreply.github.com>
2019-11-21 13:32:14 +0100

    Update ContigExtractor_install.sh

--- ContigExtractor_install.sh
+++ ContigExtractor_install.sh
@@ -1,4 +1,8 @@
+cd ContigExtractor/
 chmod a+x ContigExtractor.sh
+chmod a+x ChooseContigs.py
+chmod a+x IDFinder.py
+chmod a+x ErrorHandling.py
+chmod a+x ContigExtractor_install.sh
 
-mv ContigExtractor.sh ~/bin/
 
