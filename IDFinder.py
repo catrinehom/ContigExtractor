2db325ac5dd464d8652fc126f88eabad05f215d0
catrinehom <48596773+catrinehom@users.noreply.github.com>
2019-12-20 14:05:48 +0100

    Update IDFinder.py

--- IDFinder.py
+++ IDFinder.py
@@ -8,45 +8,11 @@ Version: 1.0
 Author: Catrine Ahrens Høm
 """
 
-# Import libraries 
+# Import libraries
 import sys
 import re
-import gzip
 from argparse import ArgumentParser
-
-###########################################################################
-# FUNCTIONS
-###########################################################################
-
-def CheckGZip(filename):
-    '''
-    This function checks if the input file is gzipped.
-    '''
-    gzipped_type = b'\x1f\x8b'
-    
-    infile = open(filename,'rb')
-    filetype = infile.read(2)
-    infile.close()
-    if filetype == gzipped_type:
-        return True
-    else:
-        return False
-    
-def OpenFile(filename,mode):
-    '''
-    This function opens the input file in chosen mode.
-    '''
-    try:
-        if CheckGZip(filename):
-            infile = gzip.open(filename,mode) 
-        else:
-            infile = open(filename,mode)   
-    except IOError as error:
-        logfile.write('Could not open '+filename+' due to: '+str(error))
-        print('Could not open '+filename+' due to: '+str(error))
-        sys.exit(1)
-    return infile
-
+from ErrorHandling import OpenFile
 
 ###########################################################################
 # GET INPUT
@@ -54,8 +20,8 @@ def OpenFile(filename,mode):
 
 # Input from command line
 parser = ArgumentParser()
-parser.add_argument('-i', dest='input',help='Input file to find IDs from')
-parser.add_argument('-o', dest='o', help='Output filename')
+parser.add_argument("-i", dest="input", help="Input file to find IDs from")
+parser.add_argument("-o", dest="o", help="Output filename")
 args = parser.parse_args()
 
 # Define input as variables
@@ -63,22 +29,19 @@ alignmentfrag = args.input
 o = args.o
 
 # Open log file
-logname = o+"/"+o+".log"
-logfile = OpenFile(logname,"a+") 
+logname = "{}/{}.log".format(o, o)
+logfile = OpenFile(logname,"a+")
 
 ###########################################################################
-# FIND IDS 
+# FIND IDS
 ###########################################################################
 
 # Define ID pattern
-#ID_pattern = b'[ATGC]+\t[0-9]+\t[0-9]+\t[0-9]+\t[0-9]+\t[a-zA-z0-9- .]*\t([a-zA-z0-9-=:]+)'
-#ID_pattern = b'([A-Za-z0-9-]*)\srunid='
-#ID_pattern = re.compile(b'[\w-]+(?=\s+runid=)')
-ID_pattern = re.compile(b'\s([\w-]+)\srunid=')
+ID_pattern = re.compile(b"\s([\w-]+)\srunid=")
 
 
 # Open input file
-infile = OpenFile(alignmentfrag,'rb')
+infile = OpenFile(alignmentfrag,"rb")
 
 # Make a set of IDs to make sure they are unique
 ID_set = set()
@@ -86,34 +49,37 @@ ID_set = set()
 # Search after ID and write dict
 for line in infile:
     ID_result = re.search(ID_pattern,line)
-    if ID_result != None: 
+    if ID_result is not None:
         ID_set.add(ID_result.group(1))
+
+# Close file
+infile.close()
 
 # Check if any ID is found
 if not ID_set:
-    print('No IDs found in '+o+alignmentfrag+'!')
-    logfile.write('No IDs found in '+o+alignmentfrag+'!')
+    message = "No IDs found in {}{}!",format(o, alignmentfrag)
+    logfile.write(message)
+    sys.exit(message)
 
 ###########################################################################
 # WRITE RESULT TO FILE
 ###########################################################################
 
 # Open output file
-outname = o+'/'+o+'_ID.txt'
-#outfile = OpenFile(outname,'w')
+outname = "{}/{}_ID.txt".format(o,o)
 
 try:
-    outfile = open(outname,'w')
+    outfile = open(outname,"w")
 except IOError as error:
-    sys.stdout.write('Could not open file due to: '+str(error))
-    sys.exit(1)
-    
+    message = "Could not open file due to: ".format(error)
+    logfile.write(message)
+    sys.exit(message)
 
-# Print ID to outfile        
-for ID in ID_set:       
-    print(ID.decode('ascii'),file=outfile)
+
+# Print ID to outfile
+for ID in ID_set:
+    print(ID.decode("ascii"), file=outfile)
 
 # Close files
-infile.close()            
 outfile.close()
 logfile.close()
